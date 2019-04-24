279271a91a204795f9c92a295aaaf60ae31778ba
zhg061 <45829527+zhg061@users.noreply.github.com>
2019-04-24 16:12:12 -0700

    updated file

---
+++ src/BinarySearchTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+public class BinarySearchTest {
+
+    @org.junit.Test
+    public void binarySearch() {
+    }
+}
---
+++ src/SortsTest.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.*;
+
+public class SortsTest {
+
+    @org.junit.Test
+    public void insertionSort() {
+    }
+
+    @org.junit.Test
+    public void mergeSort() {
+    }
+
+    @org.junit.Test
+    public void quickSort() {
+    }
+}
